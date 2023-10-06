d457794f822a78de06c9c5bf00996ccd9500bbc3
Will <wilsonarizza@gmail.com>
2023-10-06 12:19:03 -0300

    learning how to use git

---
+++ functions.js
@@ -0,0 +1,30 @@
+
+// function imc(peso, altura) {
+//     const imc = peso / (altura * 2 );
+//     return imc;
+// }
+// 
+// console.log(imc(83, 1.87));
+
+ function corFavorita(cor) {
+     if(cor === 'azul') {
+         return 'Você gosta do mar';
+     } else if (cor === 'vermelho') {
+         return 'Você é flamengo'
+     } else if (cor === 'rosa') {
+        return 'Você é menina'
+     } else if (cor === 'preto') {
+        return 'Você precisa ir na terapia'
+     } else {
+      return 'você nao gosta de cores'
+     }
+ }
+     
+ console.log(corFavorita('preto'))
+
+// function imc(peso, altura) {
+//     const imc = peso / (altura * 2);
+//     return imc;
+// }
+// 
+// console.log(imc (83, 1.87));
