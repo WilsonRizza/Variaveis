df56c1c8391b0c8d7f669390974da01a3b8cc800
Will <wilsonarizza@gmail.com>
2023-10-06 12:35:13 -0300

    sobe

--- functions.js
+++ functions.js
@@ -6,21 +6,21 @@
 // 
 // console.log(imc(83, 1.87));
 
- function corFavorita(cor) {
-     if(cor === 'azul') {
-         return 'Você gosta do mar';
-     } else if (cor === 'vermelho') {
-         return 'Você é flamengo'
-     } else if (cor === 'rosa') {
-        return 'Você é menina'
-     } else if (cor === 'preto') {
-        return 'Você precisa ir na terapia'
-     } else {
-      return 'você nao gosta de cores'
-     }
- }
-     
- console.log(corFavorita('preto'))
+// function corFavorita(cor) {
+//     if(cor === 'azul') {
+//         return 'Você gosta do mar';
+//     } else if (cor === 'vermelho') {
+//         return 'Você é flamengo'
+//     } else if (cor === 'rosa') {
+//        return 'Você é menina'
+//     } else if (cor === 'preto') {
+//        return 'Você precisa ir na terapia'
+//     } else {
+//      return 'você nao gosta de cores'
+//     }
+// }
+//     
+// console.log(corFavorita('verde'))
 
 // function imc(peso, altura) {
 //     const imc = peso / (altura * 2);
@@ -28,3 +28,23 @@
 // }
 // 
 // console.log(imc (83, 1.87));
+
+
+
+
+// function apelidosBibi(apelido) {
+//     if (apelido == "bibi") {
+//         return "você é a bibi";
+//     } else if (apelido == "pipi") {
+//         return "você é a minha pipizinha";
+//     } else if (apelido == "princesa") {
+//         return "você é a minha futura rainha";
+//     } else if (apelido == "gabi") {
+//         return "gabi não, você é pipi";
+//     } else if (apelido == "nenis") {
+//         return "nenisssssssssssss";
+//     } else {
+//         return "você errou, tente novamente"
+//     }
+// }
+// 
