05861f1d66d0b1f5ebff2951c8d8a61504b384da
Will <wilsonarizza@gmail.com>
2023-10-12 14:23:31 -0300

    for and while

---
+++ objetos.js
@@ -0,0 +1,60 @@
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+
+    cadastro = {
+        nome: 'Will',
+        sobrenome: 'Rizza',
+        idade: 27,
+        profissão: 'dev',
+    }
+
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+
+    cadastro = {
+        nome: 'Will',
+        sobrenome: 'Rizza',
+        idade: 27,
+        profissão: 'dev',
+    }
+    console.log (cadastro.nome + ' ' + cadastro.sobrenome);
+    
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+carro.preco = 3000;
+
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
+labradoodle = {
+    raça: 'labrador',
+    idade: 10,
+    latir (pessoa) {
+        if (pessoa === 'homem') {
+            return 'au au'
+        } else {
+            return '...'
+        }
+    }
+}
+
+
+for (var numero = 1 ; numero < 10; numero++) {
+    console.log(numero)
+}
+
+
+var i =2;
+while (i<10) {
+    console.log(i)
+    i++;
+}
+
+
